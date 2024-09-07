42fa1c8631be03d1312cdfb998418567ee7e5d93
Tahmina Islam <37183198+Tahmina24@users.noreply.github.com>
2024-09-07 22:39:10 +0600

    Add files via upload

---
+++ 272TextQuotes.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    long int count=0;
+    while (scanf("%c",&ch)==1)
+    {
+        if(ch=='"')
+        {
+            count++;
+            if(count%2==1)
+            {
+                printf("``");
+            }
+            else{
+                printf("''");
+            }
+
+        }
+        else{
+            printf("%c",ch);
+        }
+    }
+    return 0;
+}
